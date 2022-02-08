edd631fa2785dab5062ddbcd61f480354f2dbdd9
Seonguk <xsungwook3x@naver.com>
2022-02-08 19:54:40 +0900

    정수사각형

---
+++ 6주차/정수사각형.py
@@ -0,0 +1,16 @@
+def solution(triangle):
+    for i in range(1, len(triangle)):
+        for j in range(len(triangle[i])):
+            if j == 0:
+                triangle[i][j] += triangle[i - 1][j]
+
+            elif i == j:
+                triangle[i][j] += triangle[i - 1][-1]
+
+            else:
+                if triangle[i - 1][j - 1] > triangle[i - 1][j]:
+                    triangle[i][j] += triangle[i - 1][j - 1]
+                else:
+                    triangle[i][j] += triangle[i - 1][j]
+
+    return max(triangle[-1])
