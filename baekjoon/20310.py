43a1a61774e14ac64b48593df5b8049099aeb7e9
Seonguk <xsungwook3x@naver.com>
2024-01-24 22:11:53 +0900

    백준 20310

---
+++ baekjoon/20310.py
@@ -0,0 +1,12 @@
+li = list(input())
+zero, one = li.count('0')//2, li.count('1')//2
+for _ in range(zero):
+
+    li.pop(-li[::-1].index('0')-1)
+
+for _ in range(one):
+    li.pop(li.index('1'))
+
+print(''.join(li))
+
+
